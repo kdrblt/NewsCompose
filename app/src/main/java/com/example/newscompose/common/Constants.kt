da97a0f543d6733b408c8e3042ea96e8012b8eab
Kadir Bulut <132228860+kdrblt@users.noreply.github.com>
2023-08-09 10:50:40 +0300

    Update Constants.kt

--- app/src/main/java/com/example/newscompose/common/Constants.kt
+++ app/src/main/java/com/example/newscompose/common/Constants.kt
@@ -2,5 +2,5 @@ package com.example.newscompose.common
 
 object Constants {
     const val BASE_URL = "https://newsapi.org/v2/"
-    const val NEWS_API_KEY = "a6a64bb514174646a7c180eeadc33cd0"
+    const val NEWS_API_KEY = "7548ee493f134447bf6a5f74df749155"
 }
